9ec86fcf33543a9d30ac626bf7da8f83d7dc6e49
HenryK1577 <henklep636@gmail.com>
2025-11-09 09:36:24 -0600

    core

---
+++ flask_backend/coretest.py
@@ -0,0 +1,12 @@
+import core
+import find_nodes as fn
+
+found = core.pathfinder("Kepler-562")
+print(found)
+
+# goal = (-0.48413302824842264, -0.40481024725532766, -1.1308266333422168)
+# print(f"Attempt to find {goal}...")
+
+# p_string = fn.find_path(goal, 100)
+
+# print(p_string)
